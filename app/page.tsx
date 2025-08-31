4e1585af0d7b4950ff71e01c469579da0cab65d0
Ayush jain <ayushjain@Ayushs-MacBook-Air.local>
2025-09-01 01:18:38 +0530

    Added user management to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-export default function Page() {
+"use client";
+
+import UserTable from "./components/template/UserTable";
+
+export default function HomePage() {
   return (
-    <div className="m-4 p-4 rounded-xl border">
-      <h1 className="text-2xl font-semibold">Jumbo React FrontEnd Assignment</h1>
-      <p className="mt-2 text-sm text-gray-600 dark:text-gray-300">
-        This is a simple user management application built with React and Tailwind CSS.
-      </p>
-    </div>
+    <section className="ps-4">
+      <UserTable />
+    </section>
   );
 }
