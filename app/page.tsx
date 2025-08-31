3e3cd68e75a116e2928368127a21f5066653f046
Ayush jain <ayushjain@Ayushs-MacBook-Air.local>
2025-09-01 01:19:34 +0530

    Added activity log to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import ActivityLog from "./components/template/ActivityLog";
 import UserTable from "./components/template/UserTable";
 
 export default function HomePage() {
   return (
-    <section className="ps-4">
-      <UserTable />
-    </section>
+    <>
+      <section className="ps-4"><UserTable /></section>
+      <section className="pe-4"><ActivityLog /></section>
+    </>
   );
 }
